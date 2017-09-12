c5b6e84f030f4c38ccf6d93e018a58c5db048fe3
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-09-12 11:45:43 -0700

    sql query connection firing, need to config

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
 
+const mysql = require('mysql');
 require('dotenv').config();
-require('./lib/server.js').start();
+require('./lib/server.js').start()
+//chaining a promise here to ensure this runs after server toggle fires
+.then( () => {
+  const connection = mysql.createConnection({
+    host: process.env.DB_ENDPOINT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+  })
+
+  connection.connect( err => {
+    if(err) console.log('error:', err)
+    console.log('connected as id', connection.threadId)
+  })
+})
