282620a20588baada7362c17afb90fa17f06a32e
Will <william.f.skelton@gmail.com>
2017-09-27 12:15:42 -0700

    added other schemas

---
+++ model/contract.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const debug = require('debug')('assassintag:contract');
+const createError = require('http-errors');
+
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const contractSchema = Schema({
+  associatedGameId: {type: Schema.Types.ObjectId, required: true, unique: true}
+  assignedTo: {type: Schema.Types.ObjectId, required: true, unique: false},
+  target: {type: Schema.Types.ObjectId, required: true, unique: false},
+  isComplete: {type: Boolean, required: true, default: false},
+  dateAssigned: {type: String, required: true, unique: false},
+  dateCompleted: {type: String, required: true, unique: false}
+});
+
+module.exports = mongoose.model('contract', contractSchema);
---
+++ model/game.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const debug = require('debug')('assassintag:game');
+const createError = require('http-errors');
+
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const gameSchema = Schema({
+  adminId: {type: Schema.Types.ObjectId, ref: 'player', required: true, unique: false},
+  players: [{type: Schema.Types.ObjectId, ref: 'player', required: false, unique: false}], // List of player Ids
+  alive: [{type: Schema.Types.ObjectId, ref: 'player', required: false, unique: false}], // List of Player Ids who are alive
+  dead: [{type: Schema.Types.ObjectId, ref: 'player', required: false, unique: false}], // List of Player Ids who are dead
+  rounds: {type: Number, required: false, unique: false},
+  contracts: [{type: Schema.Types.ObjectId, ref: 'contract', required: false, unique: false}], // List of all contracts associated with this game
+});
+
+module.exports = mongoose.model('game', gameSchema);
