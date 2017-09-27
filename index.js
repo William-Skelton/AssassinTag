c0e0b46af9c7e7b08b8310df87b418fc15be35e1
Will <william.f.skelton@gmail.com>
2017-09-26 22:10:38 -0700

    added user schema and packages

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 
 require('dotenv').config();
 require('./lib/server.js').start();
-//chaining a promise here to ensure this runs after server toggle fires
--- model/user.js
+++ model/user.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+const debug = require('debug')('assassintag:user');
+
+const userSchema = Schema({
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true, unique: false},
+  findHash: {type: String, required: false, unique: true}
+});
